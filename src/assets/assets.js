740443727ea5a055e695f6b72b9f3720e96a7477
Manav Panchal <manavpanchal9462@gmail.com>
2025-08-17 22:19:54 +0530

    feat: update Navbar component to accept theme props and enhance styling

---
+++ src/assets/assets.js
@@ -0,0 +1,58 @@
+
+
+
+
+
+
+export const company_logos = [
+    microsoft_logo,
+    zoom_logo,
+    rakuten_logo,
+    coinbase_logo,
+    airbnb_logo,
+    google_logo,
+]
+
+const assets = {
+    logo,
+    arrow_icon,
+    group_profile,
+    bgImage1,
+    bgImage2,
+    hero_img,
+    ads_icon,
+    content_icon,
+    marketing_icon,
+    social_icon,
+    menu_icon,
+    close_icon,
+    work_mobile_app,
+    work_fitness_app,
+    work_dashboard_management,
+    email_icon,
+    person_icon,
+    facebook_icon,
+    twitter_icon,
+    linkedin_icon,
+    logo_dark,
+    menu_icon_dark,
+    sun_icon,
+    moon_icon
+}
+
+export default assets
+
+export const teamData = [
+    {
+        name: 'Harry Rathod', title: 'Founder & CEO', image: ''
+    },
+    {
+        name: 'Manav Panchal', title: 'Ads manager', image: ''
+    },
+    {
+        name: 'Siddharth Rathod', title: 'Vice president', image: ''
+    },
+    {
+        name: 'Rishabh Rathod', title: 'Co-Founder & COO', image: ''
+    }
+]
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = ({theme, setTheme}) => {
   return (
-    <div className='flex justify-between items-center px-4 sm:px-24  xl:px-24'>Navbar</div>
+    <div className='flex justify-between items-center px-4 sm:px-24 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70'>Navbar</div>
+    <img src=''
   )
 }
 
