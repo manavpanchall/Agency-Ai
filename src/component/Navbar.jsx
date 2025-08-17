afa6a22611af8124fa38d6f80a2acc10922f5488
Manav Panchal <manavpanchal9462@gmail.com>
2025-08-17 21:53:37 +0530

    feat: add Navbar component with responsive layout

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center px-4 sm:px-24  xl:px-24'>Navbar</div>
+  )
+}
+
+export default Navbar
