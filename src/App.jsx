1f60ef4457bbd50f00d925e3349b4149d36c7f7b
Manav Panchal <manavpanchal9462@gmail.com>
2025-08-28 10:44:25 +0530

    Add Zoom logo asset to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { use, useState } from 'react'
+import Navbar from './component/Navbar'
 
 const App = () => {
+
+  cosnt [theme, setTheme] = useState('light')
+
   return (
-    <div>App</div>
+    <div className='dark:bg-black relative'>
+      <Navbar theme={theme} setTheme={setTheme} />
+    </div>
   )
 }
 
