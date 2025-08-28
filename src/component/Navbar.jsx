f54d0e267c3411b3fbc7a9f07a43b1a692e41a3d
Manav Panchal <manavpanchal9462@gmail.com>
2025-08-28 11:34:33 +0530

    feat: implement theme toggle functionality with ThemeToggleBtn component; update App and Navbar components to support theme state

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import assets from '../assets/assets'
+import ThemeToggleBtn from './ThemeToggleBtn'
 
 const Navbar = ({theme, setTheme}) => {
 
@@ -18,6 +19,8 @@ const Navbar = ({theme, setTheme}) => {
       </div>
 
       <div className='flex items-center gap-2 sm:gap-4' >
+
+        <ThemeToggleBtn theme={theme} setTheme={setTheme} />
 
         <img src={theme === 'dark' ? assets.menu_icon_dark : assets.menu_icon} alt="" onClick={()=> setSiderbarOpen(true)} className='w-8 sm:hidden' />
         <a href="#contact-us" className='text-sm max-sm:hidden flex items-center gap-2 bg-primary text-white px-6 py-2 rounded-full cursor-pointer hover:scale-103 transition-all'>
